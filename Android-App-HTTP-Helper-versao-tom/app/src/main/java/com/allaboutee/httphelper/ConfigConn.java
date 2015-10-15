69211fc231d47135ca912437758ba8c4c6e5482c
isabela-angelo <isabela.angelo@gmail.com>
2015-10-15 09:41:59 -0300

    atividade 2

--- Android-App-HTTP-Helper-versao-tom/app/src/main/java/com/allaboutee/httphelper/ConfigConn.java
+++ Android-App-HTTP-Helper-versao-tom/app/src/main/java/com/allaboutee/httphelper/ConfigConn.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
@@ -27,8 +28,12 @@ import java.net.URISyntaxException;
 
 public class ConfigConn extends HomeActivity{
 
+    public final static String PREF_NAME = "PREF_NAME";
     private Button button_SET;
-    private EditText editTextSSID, editTextsenha;
+    private EditText editTextSSID, editTextsenha, editTextnome;
+    //SharedPreferences sharedPreferences;
+    //SharedPreferences.Editor editor;
+    //SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +42,7 @@ public class ConfigConn extends HomeActivity{
 
         editTextSSID = (EditText)findViewById(R.id.eg_ssid);
         editTextsenha = (EditText)findViewById(R.id.eg_senha);
+        editTextnome = (EditText)findViewById(R.id.eg_nome);
 
         button_SET = (Button)findViewById(R.id.button_SET);
         button_SET.setOnClickListener(this);
@@ -50,17 +56,21 @@ public class ConfigConn extends HomeActivity{
             String ssid = editTextSSID.getText().toString().trim();
             // get the port number
             String senha = editTextsenha.getText().toString().trim();
-            parameterValue = ssid+senha;
+            String nome = editTextnome.getText().toString().trim();
+            parameterValue = "ssid="+ssid+"+senha="+senha+"+nome="+nome;
             String ipAddress = "192.168.4.1";
             String portNumber = "80";
             // execute HTTP request
             if (ssid.length() > 0 && senha.length() > 0) {
                 new HttpRequestAsyncTask(
-                        view.getContext(), "="+parameterValue, ipAddress, ":"+portNumber, "/?info"
+                        view.getContext(), "="+parameterValue, ipAddress, ":"+portNumber, "/?"
                 ).execute();
             }
+            editor = sharedPreferences.edit();
+            // save the name for the next time the app is used
+            editor.putString(PREF_NAME, nome);
+            editor.commit(); // save name
         }
-
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
